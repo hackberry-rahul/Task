48b7be9bcfb3ea4e18d0683fe7ec6be80e62657b
patel meet <meet20043@gmail.com>
2022-12-07 17:02:50 +0530

    php up

---
+++ Meet/php tast/goto-statement.php
@@ -0,0 +1,16 @@
+<?php
+echo "welcome to a php";
+echo "welcome to a php";
+echo "welcome to a php";
+
+goto abc;
+echo "welcome to a php";
+echo "welcome to a php";
+echo "welcome to a php";
+echo "welcome to a php";
+echo "welcome to a php";
+echo "welcome to a php";
+
+abc:
+echo "hey this is lebal abc";
+?>
