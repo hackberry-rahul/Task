2b6a3807619d3e161a98dbd09e77980ec782589b
yashpatel <yashpatel0197@gmail.com>
2022-12-26 11:25:13 +0530

    finxpert php add

---
+++ Yash/finxpert in php/partials/footer.php
@@ -0,0 +1,105 @@
+
+        <!-- start site-footer -->
+        <footer class="site-footer">
+            <div class="upper-footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col col-md-4 col-sm-6">
+                            <div class="widget about-widget">
+                                <div class="footer-logo" style="background-color:#fff;"><img
+                                        src="assets/images/slider/logo1.png" alt></div>
+                                <p>We are Providing All kind of Secured and Loans from various financial institutions.
+                                </p>
+                                <ul class="contact-info">
+                                    <li><i class="fa fa-phone"></i> <a href="tel:+917965558888">+91 79655-58888</a></li>
+                                    <li><i class="fa fa-envelope"></i> info@finXpert.in</li>
+                                    <li><a href="https://goo.gl/maps/zQL4r8JUSyPb4MZR8" target="_blank"><i class="fa fa-home"></i>104,Akash Complex,C.G.Road, Ahemdabad.</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col col-md-4 col-sm-6">
+                            <div class="widget links-widget">
+                                <h3>Links</h3>
+                                <ul>
+                                    <li><a href="#">Home</a></li>
+                                    <li><a href="services.php">Services</a></li>
+                                    <li><a href="about.php">About us</a></li>
+                                    <li><a href="#">EMI-Calculator</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                            
+
+                        <div class="col col-md-4 col-sm-6">
+                            <div class="widget twitter-feed-widget">
+                                <h3>Twitter Feed</h3>
+                                <ul>
+                                    <li>
+                                        <div class="text">
+                                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+                                                fugit. Ed quia con sequuntur magni dolores.</p>
+                                        </div>
+                                        <div class="info-box">
+                                            <i class="fa fa-twitter"></i>
+                                            <strong><a href="#">@Gaurav Madhuwala</a></strong>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="text">
+                                            <p>Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
+                                                fugit. Ed quia con sequuntur magni dolores.</p>
+                                        </div>
+                                        <div class="info-box">
+                                            <i class="fa fa-twitter"></i>
+                                            <strong><a href="#">@Vivek Swami</a></strong>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- end upper-footer -->
+            <div class="copyright-info">
+                <div class="container">
+                    <div class="row">
+                        <div class="col col-xs-6">
+                            <div class="copyright-area">
+                                <p>2015 &copy; All Rights Reserved by FinXpert</a></p>
+                            </div>
+                        </div>
+                        <div class="col col-xs-6">
+                            <div class="footer-social">
+                                <span>Follow us: </span>
+                                <ul class="social-links">
+                                    <li><a href="https://www.facebook.com/finxpertin/"><i
+                                                class="fa fa-facebook-square"></i></a></li>
+                                    <li><a href="https://twitter.com/CONTACTFINXPERT"><i
+                                                class="fa fa-twitter-square"></i></a></li>
+                                    <li><a
+                                            href="https://www.linkedin.com/company/finxpert-a-complete-capital-solution/"><i
+                                                class="fa fa-linkedin-square"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end site-footer -->
+
+           <!-- All JavaScript files
+    ================================================== -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <!-- Plugins for this template -->
+    <script src="assets/js/jquery-plugin-collection.js"></script>
+
+     <!-- Google map api -->
+     <script src="https://maps.googleapis.com/maps/api/js?key"></script>    
+
+<!-- Custom script for this template -->
+<script src="assets/js/script.js"></script>
